512737488a8950e02f60793491c178a3cc7ff449
laijingquan <351779036@qq.com>
2018-01-07 18:51:32 +0800

    1.vox的颜色也解析出来了 2.导入笔刷插件

--- Assets/Scripts/GenerateVoxel.cs
+++ Assets/Scripts/GenerateVoxel.cs
@@ -8,18 +8,25 @@ public class GenerateVoxel : MonoBehaviour {
     MainData MD;
     private void Awake()
     {
-        var text = Resources.Load<TextAsset>("menger").text;
+        var text = Resources.Load<TextAsset>("castle").text;
         MD = LitJson.JsonMapper.ToObject<MainData>(text);
         //var data = Resources.Load()
     }
     // Use this for initialization
     void Start ()
     {
+        
         var voxDatas = MD.voxDatas;
+        var rgbas = MD.rgbas;
+        var root = new GameObject();
         for(int i =0; i < voxDatas.Count;i++)
         {
             var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
             obj.transform.position = new Vector3(voxDatas[i].x, voxDatas[i].y, voxDatas[i].z);
+            obj.transform.SetParent(root.transform);
+            var colorIndex = voxDatas[i].colorIndex-1;
+
+            obj.GetComponent<Renderer>().material.color = new Color(rgbas[colorIndex].r/255f,rgbas[colorIndex].g/255f,rgbas[colorIndex].b/255f,rgbas[colorIndex].a/255f);
         }
 
     }
@@ -33,6 +40,7 @@ public class GenerateVoxel : MonoBehaviour {
 public class MainData
 {
     public List<VoxData> voxDatas;
+    public List<RGBA> rgbas;
 }
 
 public class VoxData
@@ -41,5 +49,13 @@ public class VoxData
     public int y;
     public int z;
     public int colorIndex;
+}
+
+public class RGBA
+{
+    public int r;
+    public int g;
+    public int b;
+    public int a;
 }
 
